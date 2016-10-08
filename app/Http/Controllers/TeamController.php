23f4738f0dd3d4eba1b5c2946e3a03b91380e626
Umut Yerebakmaz <umutyerebakmaz@hotmail.com>
2016-10-08 17:38:47 +0300

    .gitignore updated

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -24,38 +24,43 @@ class TeamController extends Controller
             ->withData($data);
     }
 
-    // new team member create page
+    // create new team member page
     public function create()
     {
         return view('backend.team.create', compact('data'));
     }
 
+    // store new team member
     public function store(Request $request)
     {
+        // with validations
         $validator = Validator::make($request->all(), [
             'adsoyad' => 'required',
             'unvan' => 'required',
             'fotograf' => 'required'
         ]);
 
+        // return with errors
         if ($validator->fails()) {
             return redirect('admin/team/create')
                 ->withErrors($validator)
                 ->withInput();
         }
 
+        // no errors do it
         $team = new Team;
         $team->name = trim($request->adsoyad);
         $team->title = trim($request->unvan);
         $team->image = $request->fotograf;
-
         $team->save();
 
+        // if team stored return admin/team page
         if($team->save()) {
             return redirect('admin/team');
         }
     }
-    
+
+    // team members ajax update
     public function update(Request $request, $id)
     {
         foreach( $request->get('data') as $key => $id){
@@ -63,10 +68,11 @@ class TeamController extends Controller
             $team->order_id = $key;
             $team->save();
         }
+        // when update order_id refresh order_id
         return $request->get('data');
     }
 
-    // delete function 
+    // team members single and multiple checkbox deleting from ajax
     public function destroy(Request $request)
     {
         foreach( $request->get('data') as $key => $id){
